335849475948577b8e65e69239d2a69c0a4203a0
aysunhp <bd7zatzha@code.edu.az>
2025-12-12 11:12:15 +0000

    task7 updated

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -2,5 +2,4 @@
 def simple_delete(a_dictionary, key=""):
     if key in a_dictionary.keys():
         del a_dictionary[key]
-        for key in a_dictionary:
-            print("{}: {}".format(key, a_dictionary[key]))
+    return a_dictionary
