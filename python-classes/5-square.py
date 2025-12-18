6936cee202ef55f797486aa9d0a6fe5383a856a6
aysunhp <bd7zatzha@code.edu.az>
2025-12-18 12:27:26 +0000

    task5 updated

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -34,5 +34,5 @@ class Square():
         if self.__size == 0:
             print("")
         else:
-            for i in range(1, self.__size):
+            for i in range(0, self.__size):
                 print("#"*self.__size)
