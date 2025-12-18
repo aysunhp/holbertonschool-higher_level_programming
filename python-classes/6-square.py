95fe3ac83954e4f1a7adbec6f2d8e95f3b255f38
aysunhp <bd7zatzha@code.edu.az>
2025-12-18 12:46:31 +0000

    task6 addedd

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -39,8 +39,9 @@ class Square():
     @position.setter
     def position(self, value):
         "This is position's setter function"
-        if not isinstance(value, tuple(int,int)):
-            raise TypeError("position must be a tuple of 2 positive integers")
+        if not(isinstance(value, tuple) and len(self.__position)==2):
+            if not all(isinstance(element, int) or element < 0 for element in variable):
+                raise TypeError("position must be a tuple of 2 positive integers")
         if value < 0:
             raise ValueError("size must be >= 0")
 
