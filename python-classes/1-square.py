183a0cae31aae4279ced9812529890921077b180
aysunhp <bd7zatzha@code.edu.az>
2025-12-18 10:59:44 +0000

    task1 added

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module contains a class and functions to manage geometric shapes.
+"""
+
+
+class Square():
+    """Defines a square"""
+    __size = 3
+
+    def __init__(self, size):
+        """This function is constructor"""
+        self.__size = size
