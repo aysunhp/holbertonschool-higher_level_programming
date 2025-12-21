1b5faff0361f5e1896e1793b851e56ca76e0ba62
aysunhp <bd7zatzha@code.edu.az>
2025-12-21 16:18:37 +0000

    task6 updateddd

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -4,12 +4,13 @@
 
 class Rectangle:
     """Rectange defining class"""
+
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.__width = width
         self.__height = height
-        self.number_of_instances += 1
-        
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -58,5 +59,5 @@ class Rectangle:
         return f"Rectangle({self.__width}, {self.__height})"
 
     def __del__(self):
-        self.number_of_instances -= 1
+        Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
