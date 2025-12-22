b45947807acee0a8ac9205869063928e38f6010a
aysunhp <bd7zatzha@code.edu.az>
2025-12-22 12:18:49 +0000

    task0 added

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Python inheritance module"""
+
+
+def lookup(obj):
+    """This functions returns list of
+    available attributes and methods of an object"""
+
+    return dir(obj)
