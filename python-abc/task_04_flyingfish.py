baec84022dbb57d97ae9471018479a2859b3cca5
aysunhp <bd7zatzha@code.edu.az>
2026-01-02 17:30:00 +0000

    task4 added

---
+++ python-abc/task_04_flyingfish.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Python abstract module"""
+
+
+class Fish():
+    """This is Fish class with swim and habitat method"""
+    def swim(self):
+        print("The fish is swimming")
+
+    def habitat(self):
+        print("The fish lives in water")
+
+
+class Bird():
+    """This is Bird class with fly and habitat method"""
+    def fly(self):
+        print("The bird is flying")
+
+    def habitat(self):
+        print("The fish lives in sky")
+
+
+class FlyingFish(Fish, Bird):
+    """This  class is inherited from Bird and Fish classes"""
+    def fly(self):
+        print("The flying fish is soaring")
+
+    def swim(self):
+        print("The flying fish is swimming!")
+
+    def habitat(self):
+        print("The flying fish lives both in water and the sky!")
