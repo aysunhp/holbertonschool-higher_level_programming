05578867e17c0ea01cca56ede1b83238d7149f22
aysunhp <bd7zatzha@code.edu.az>
2026-01-02 22:09:12 +0000

    task11 added

---
+++ python-input_output/11-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This is input-output module"""
+
+
+class Student:
+    """This is student class"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
