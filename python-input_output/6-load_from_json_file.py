59b6307591e2fcc5b80ef7e0ac1e4b8060351d81
aysunhp <bd7zatzha@code.edu.az>
2026-01-02 21:16:31 +0000

    task6 added

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This is input-output module"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """This function  creates an Object from a JSON file"""
+
+    with open(filename, "r") as file:
+        return json.load(f)
